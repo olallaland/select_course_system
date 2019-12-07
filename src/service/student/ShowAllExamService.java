9fa1ae6e712151537f99758e1b76fb464fa8798c
olallaland <397568441@qq.com>
2019-12-07 13:53:10 +0800

    some update

--- src/service/student/ShowAllExamService.java
+++ src/service/student/ShowAllExamService.java
@@ -1,11 +1,19 @@
 package service.student;
 
+import bean.ApplyBean;
+import bean.EvaluationBean;
+import bean.UserBean;
+import dao.CourseDao;
+import dao.EvaluationDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * ? ?
@@ -17,7 +25,28 @@ import java.io.IOException;
 public class ShowAllExamService extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // get student id
+        HttpSession session = request.getSession();
+        UserBean user = (UserBean)session.getAttribute("user");
+        String studentId = user.getId();
+        EvaluationDao evaluation = new EvaluationDao();
+        ArrayList<EvaluationBean> examList = new ArrayList<EvaluationBean>();
+        String error = "";
 
+        try {
+            examList = evaluation.showAllEvaluation(studentId);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            error = e.getMessage();
+        }
+
+        if (!error.equals("")) {
+            request.setAttribute("error", error);
+            request.getRequestDispatcher("/student.jsp").forward(request, response);
+        }else {
+            request.setAttribute("examList", examList);
+            request.getRequestDispatcher("/student.jsp").forward(request, response);
+        }
     }
 
     @Override
